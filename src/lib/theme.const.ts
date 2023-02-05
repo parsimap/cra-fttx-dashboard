8adc77b102f6075e8e1750e681de383eb324b934
Muhamad Zolfaghari <muhamad.zolfaghari@gmail.com>
2023-02-05 14:37:10 +0330

    The theme was updated.

---
+++ src/lib/theme.const.ts
@@ -0,0 +1,53 @@
+import { createTheme } from "@mui/material/styles";
+import { faIR } from "@mui/material/locale";
+
+const theme = createTheme(
+  {
+    direction: "rtl",
+    components: {
+      MuiButton: {
+        styleOverrides: {
+          root: {
+            borderRadius: 20,
+          },
+        },
+      },
+      MuiToggleButtonGroup: {
+        styleOverrides: {
+          root: {
+            borderRadius: 20,
+          },
+        },
+      },
+      MuiToggleButton: {
+        styleOverrides: {
+          root: {
+            borderRadius: 20,
+          },
+        },
+      },
+    },
+    palette: {
+      primary: {
+        // light: "#7ccad8",
+        // main: "#8C0D22",
+        // dark: "#151348",
+      },
+      secondary: {
+        // main: "#FF7A00",
+        // light: "#FFA95B",
+        // dark: "#E85A02",
+        // contrastText: "#ffffff",
+      },
+      text: {
+        primary: "#444444",
+      },
+    },
+    typography: {
+      fontFamily: "Vazirmatn, Quicksand",
+    },
+  },
+  faIR
+);
+
+export default theme;
