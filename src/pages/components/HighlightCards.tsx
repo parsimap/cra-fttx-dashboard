4130ce4c1a96d66b9c069996be72034e32a73ab6
Muhamad Zolfaghari <muhamad.zolfaghari@gmail.com>
2023-02-08 19:21:52 +0330

    HighlightCards.tsx was added.

--- src/pages/components/HighlightCards.tsx
+++ src/pages/components/HighlightCards.tsx
@@ -6,21 +6,22 @@ import IHighlightItem from "@/src/interfaces/IHighlightItem";
 const HighlightCard = ({ title, subtitle }: IHighlightItem) => (
   <Box
     sx={{
-      borderRadius: 5,
       p: 3,
       width: 250,
+      borderRadius: 5,
+      color: "#ffffff",
       filter: "drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))",
       background:
         "linear-gradient(180deg, #2D346C 0%, rgba(0, 73, 181, 0.7) 100%)",
     }}
   >
-    <Typography variant={"h3"}>{title}</Typography>
+    <Typography fontSize={20}>{title}</Typography>
     <Typography variant={"subtitle2"}>{subtitle}</Typography>
   </Box>
 );
 
 const HighlightCards = ({ data }: { data: IHighlightItem[] }) => (
-  <Stack>
+  <Stack direction={"row"} spacing={1}>
     {data.map((item, key) => (
       <HighlightCard
         key={key}
